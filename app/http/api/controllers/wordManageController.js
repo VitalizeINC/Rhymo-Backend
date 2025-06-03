325e8ad29c245732ddc7f631ca32721362c0b1c8
F4RAN <parsa.hassani30@gmail.com>
2025-06-03 16:51:25 +0330

    ss

--- app/http/api/controllers/wordManageController.js
+++ app/http/api/controllers/wordManageController.js
@@ -180,7 +180,7 @@ class wordManageController extends controller {
         let words = await Word.find({ avaString: searchAva, word: searchChar }).select('ava avaString word spacePositions nimFaselehPositions fullWord heja hejaCounter');
         // Remove words with more than rhymeHeja from result
         console.log("rhymeHeja", rhymeHeja, backupFilterAva.length)
-        for(let i = rhymeHeja; i < backupFilterAva.length; i++){
+        for(let i = rhymeHeja; i <= backupFilterAva.length; i++){
             let newFilterAva = Object.assign([], backupFilterAva)
             let newSearchAva = new RegExp(newFilterAva.splice( newFilterAva.length - (i + 1) , newFilterAva.length ).join(","));
             // console.log("newSearchAva", newSearchAva)
